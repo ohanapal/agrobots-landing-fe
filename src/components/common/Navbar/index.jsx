b98f20035f10c812d13bc5bef86a89fa16815702
mahi1212 <mahi121.mr@gmail.com>
2024-10-09 17:18:04 +0600

    logo dark mood bug resolved

--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -1,6 +1,7 @@
 'use client'
 
-import logo from '@/assets/images/ui/logo.png'
+import logoWhite from '@/assets/images/ui/logo-white.png'
+import logoDark from '@/assets/images/ui/logo.png'
 import SignupButtons from '@/components/common/Navbar/SignupButtons'
 import { cn } from '@/lib/utils'
 import { AlignRight } from 'lucide-react'
@@ -9,6 +10,7 @@ import { Img } from '../../ui/img'
 import LLink from '../../ui/llink'
 import ThemeSwitcher from '../ThemeSwitcher'
 import MobileNavbar from './MobileNavbar'
+import { useTheme } from 'next-themes'
 //import SignupButtons from './SignupButtons'
 
 export default function Navbar({ t }) {
@@ -27,13 +29,14 @@ export default function Navbar({ t }) {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [])
+  const { theme } = useTheme()
 
   return (
     <>
       <header className={cn('fixed w-full top-0 left-0 right-0 h-20 backdrop-blur-lg z-40', { 'bg-white/50': !isTop })}>
         <nav className='max-w-[1800px] mx-auto  flex items-center justify-between h-full'>
           <LLink href='/'>
-            <Img src={logo} alt='Argobots' className='w-auto h-1/2' />
+            <Img src={theme === 'dark' && logoWhite ? logoWhite : logoDark} alt='Argobots' className='w-auto h-1/2' />
           </LLink>
           {/* <ul className='hidden lg:flex gap-x-5 text-text-secondary font-medium'>
             {t.navLinks?.map(link => (
